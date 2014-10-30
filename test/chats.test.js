799bf59bc079492741d913b8f4139305a669da14
zkf9971 <zkf9971@gmail.com>
2014-10-30 23:36:34 +0800

    Changes of Nodejs-1705. Objects chats, checkins, events, and emails.

---
+++ test/chats.test.js
@@ -0,0 +1,217 @@
+var assert = require('assert'),
+    testUtil = require('./testUtil');
+
+var acsKey = process.env.ACS_APPKEY;
+if (!acsKey) {
+    console.error('Please create an ACS app and assign ACS_APPKEY in environment vars.');
+    process.exit(1);
+}
+console.log('MD5 of ACS_APPKEY: %s', testUtil.md5(acsKey));
+
+var acsApp = require('../index')(acsKey),
+    acsUsername_1 = null,
+    acsPassword = 'cocoafish',
+    acsUserCount = 0,
+    acsUsername_2 = null,
+    message = "Node ACS SDK Redesign Test - chats",
+    acsUser1_id = null,
+    chat_group_id = null,
+    chat_id = null;
+
+
+describe('Chats Test', function() {
+    before(function(done) {
+        testUtil.generateUsername(function(username) {
+            acsUsername_1 = username;
+            console.log('\tGenerated acs user 1: %s', acsUsername_1);
+            testUtil.generateUsername(function(username) {
+                acsUsername_2 = username;
+                console.log('\tGenerated acs user 2: %s', acsUsername_2);
+                done();
+            });
+        });
+    });
+
+    describe('Test chats ', function() {
+        it('Should create user 1 successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersCreate({
+                username: acsUsername_1,
+                password: acsPassword,
+                password_confirmation: acsPassword
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'createUser');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername_1);
+                assert(result.cookieString);
+                acsUser1_id = result.body.response.users[0].id
+                done();
+            });
+        });
+
+        it('Should create user 2 successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersCreate({
+                username: acsUsername_2,
+                password: acsPassword,
+                password_confirmation: acsPassword
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'createUser');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername_2);
+                assert(result.cookieString);
+                done();
+            });
+        });
+
+        it('User 2 should be able to login successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersLogin({
+                login: acsUsername_2,
+                password: acsPassword
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'loginUser');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername_2);
+                assert(result.cookieString);
+                assert.equal(typeof result.cookieString, 'string');
+                acsApp.setSessionByCookieString(result.cookieString);
+                assert.equal(result.cookieString, acsApp.appOptions.cookieString);
+                done();
+            });
+        });
+
+        it('Should send message to user 1 successfully - chats/create.json', function(done) {
+            this.timeout(20000);
+            acsApp.chatsCreate({
+                to_ids: acsUser1_id,
+                message: message
+            }, function(err, result) {
+                console.log(JSON.stringify(result.body));
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'createChatMessage');
+                assert(result.body.response);
+                assert(result.body.response.chats);
+                assert(result.body.response.chats[0]);
+                assert.equal(result.body.response.chats[0].message, message);
+                assert(result.cookieString);
+                chat_group_id = result.body.response.chats[0].chat_group_id;
+                chat_id = result.body.response.chats[0].id;
+                done();
+            });
+        });
+
+        it('Should query chats successfully - chats/query.json', function(done) {
+            this.timeout(20000);
+            acsApp.chatsQuery({
+                participate_ids: acsUser1_id
+            }, function(err, result) {
+                console.log(JSON.stringify(result.body));
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'queryChatMessages');
+                assert(result.body.response);
+                assert(result.body.response.chats);
+                assert(result.body.response.chats[0]);
+                assert.equal(result.body.response.chats[0].message, message);
+                assert(result.cookieString);
+                done();
+            });
+        });
+
+        it('Should get chat groups that user 1 participates in successfully - chats/get_chat_groups', function(done) {
+            this.timeout(20000);
+            acsApp.chatsGetChatGroups({
+
+            }, function(err, result) {
+                console.log(JSON.stringify(result.body));
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'getChatGroups');
+                assert(result.body.response);
+                assert(result.body.response.chat_groups);
+                assert(result.body.response.chat_groups[0]);
+                assert.equal(result.body.response.chat_groups[0].message, message);
+                assert(result.cookieString);
+                done();
+            });
+        });
+
+        it('Should get the count of chats successfully - chats/count.json', function(done) {
+            this.timeout(20000);
+            acsApp.chatsCount(function(err, result) {
+                console.log(JSON.stringify(result.body));
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'chatsCount');
+                console.log('\tCurrent chats count: %s', result.body.meta.count);
+                done();
+            });
+        });
+
+        it('Should delete chat that user 1 participates in successfully - chats/delete.json', function(done) {
+            this.timeout(20000);
+            acsApp.chatsDelete({
+                chat_id: chat_id
+            }, function(err, result) {
+                console.log(JSON.stringify(result.body));
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'deleteChat');
+                done();
+            });
+        });
+    });
+
+//    describe('.createUser user 2', function() {
+//        it('Should create user 2 successfully', function(done) {
+//            this.timeout(20000);
+//            acsApp.usersCreate({
+//                username: acsUsername_2,
+//                password: acsPassword,
+//                password_confirmation: acsPassword
+//            }, function(err, result) {
+//                assert.ifError(err);
+//                assert(result.body);
+//                assert(result.body.meta);
+//                assert.equal(result.body.meta.code, 200);
+//                assert.equal(result.body.meta.method_name, 'createUser');
+//                assert(result.body.response);
+//                assert(result.body.response.users);
+//                assert(result.body.response.users[0]);
+//                assert.equal(result.body.response.users[0].username, acsUsername_2);
+//                assert(result.cookieString);
+//                done();
+//            });
+//        });
+//    });
+});
---
+++ test/events.test.js
@@ -0,0 +1,175 @@
+var assert = require('assert'),
+    testUtil = require('./testUtil');
+
+var acsKey = process.env.ACS_APPKEY;
+if (!acsKey) {
+    console.error('Please create an ACS app and assign ACS_APPKEY in environment vars.');
+    process.exit(1);
+}
+console.log('MD5 of ACS_APPKEY: %s', testUtil.md5(acsKey));
+
+var acsApp = require('../index')(acsKey),
+    acsUsername = null,
+    acsPassword = 'cocoafish',
+    acsUserCount = 0;
+
+
+describe('Events Test', function() {
+    before(function(done) {
+        testUtil.generateUsername(function(username) {
+            acsUsername = username;
+            console.log('\tGenerated acs user: %s', acsUsername);
+            done();
+        });
+    });
+
+    describe('create event', function() {
+        it('Should create user successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersCreate({
+                username: acsUsername,
+                password: acsPassword,
+                password_confirmation: acsPassword
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'createUser');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername);
+                assert(result.cookieString);
+                acsApp.setSessionByCookieString(result.cookieString);
+                done();
+            });
+        });
+
+        it('Should create an event', function(done) {
+            this.timeout(20000);
+            acsApp.eventsCreate({
+                name: 'Test - events',
+//                start_time: '2011-03-22T20:59:50+0000',
+                start_time: new Date(),
+                duration: 3
+            },function(err, result) {
+                assert.ifError(err);
+                console.log(JSON.stringify(result.body));
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert(result.body.response);
+                assert(result.body.response.events);
+                assert.equal(typeof result.body.response.events, 'number');
+                console.log('\tCurrent events count: %s', result.body.response.events);
+
+                done();
+            });
+        });
+
+    });
+
+    describe('.loginUser', function() {
+        it('Newly created user should be able to login successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersLogin({
+                login: acsUsername,
+                password: acsPassword
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'loginUser');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername);
+                assert(result.cookieString);
+                assert.equal(typeof result.cookieString, 'string');
+                acsApp.setSessionByCookieString(result.cookieString);
+                assert.equal(result.cookieString, acsApp.appOptions.cookieString);
+                done();
+            });
+        });
+
+        it('Should show logged in user correctly with stored cookie string', function(done) {
+            this.timeout(20000);
+            acsApp.usersShowMe(function(err, result) {
+                assert.ifError(err);
+                assert(result);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'showMe');
+                assert(result.body.response);
+                assert(result.body.response.users);
+                assert(result.body.response.users[0]);
+                assert.equal(result.body.response.users[0].username, acsUsername);
+                done();
+            });
+        });
+    });
+
+//    describe('.updateUser', function() {
+//        it('Should update user successfully with custom_fields as a hash', function(done) {
+//            this.timeout(20000);
+//            acsApp.usersUpdate({
+//                custom_fields: {
+//                    test: true
+//                }
+//            }, function(err, result) {
+//                assert.ifError(err);
+//                assert(result);
+//                assert(result.body);
+//                assert(result.body.meta);
+//                assert.equal(result.body.meta.code, 200);
+//                assert.equal(result.body.meta.method_name, 'updateUser');
+//                assert(result.body.response);
+//                assert(result.body.response.users);
+//                assert(result.body.response.users[0]);
+//                assert(result.body.response.users[0].custom_fields);
+//                assert(result.body.response.users[0].custom_fields.test);
+//                assert.equal(result.body.response.users[0].custom_fields.test, true);
+//                done();
+//            });
+//        });
+//    });
+
+    describe('.deleteUser', function() {
+        it('Should delete current user successfully', function(done) {
+            this.timeout(20000);
+            acsApp.usersRemove(function(err, result) {
+                assert.ifError(err);
+                assert(result);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'deleteUser');
+                done();
+            });
+        });
+
+        it('User count should be decreased', function(done) {
+            this.timeout(20000);
+            // Delayed job and need time to wait for
+            setTimeout(function() {
+                acsApp.usersCount(function(err, result) {
+                    assert.ifError(err);
+                    assert(result.body);
+                    assert(result.body.meta);
+                    assert.equal(result.body.meta.code, 200);
+                    // A bug of https://jira.appcelerator.org/browse/CLOUDSRV-4022
+                    // assert.equal(result.body.meta.method_name, 'countUser');
+                    assert(result.body.response);
+                    assert(result.body.response.users);
+                    assert.equal(typeof result.body.response.users, 'number');
+                    console.log('\tCurrent users count: %s', result.body.response.users);
+                    assert.equal(result.body.response.users, acsUserCount);
+                    done();
+                });
+            }, 2000);
+        });
+    });
+});
