435ee59b6f87c7fca59f3910c1a35fbf8f48b7ba
Eddie Liu <gliu@appcelerator.com>
2014-11-06 01:31:56 +0800

    NODEJS-1751 ACS Node SDK Rework Part 2

---
+++ test/clients.test.js
@@ -0,0 +1,102 @@
+var assert = require('assert'),
+    testUtil = require('./testUtil'),
+    fs = require('fs');
+
+var acsKey = "ISiYrQsWaMxcT5rDDPhqm6NgxCELVsCF"; //process.env.ACS_APPKEY;
+var acsEntryPoint = 'http://localhost:8082'; //(process.env.ACS_ENTRYPOINT ? process.env.ACS_ENTRYPOINT : 'https://api.cloud.apcelerator.com');
+if (!acsKey) {
+    console.error('Please create an ACS app and assign ACS_APPKEY in environment vars.');
+    process.exit(1);
+}
+console.log('MD5 of ACS_APPKEY: %s', testUtil.md5(acsKey));
+
+var acsApp = require('../index')(acsKey, {
+    apiEntryPoint: acsEntryPoint
+}),
+    acsUsername = null,
+    acsPassword = 'cocoafish',
+    acsUserCount = 0,
+    acsPhotoCount = 0;
+
+var timeout = 50000;
+
+describe('Photos Test', function() {
+    this.timeout(timeout);
+
+    before(function(done) {
+        testUtil.generateUsername(function(username) {
+            acsUsername = username;
+            console.log('\tGenerated acs user: %s', acsUsername);
+            done();
+        });
+    });
+
+    before(function(done) {
+        acsApp.usersCreate({
+            username: acsUsername,
+            password: acsPassword,
+            password_confirmation: acsPassword
+        }, function(err, result) {
+            assert.ifError(err);
+            assert(result.body);
+            assert(result.body.meta);
+            assert.equal(result.body.meta.code, 200);
+            assert.equal(result.body.meta.method_name, 'createUser');
+            assert(result.body.response);
+            assert(result.body.response.users);
+            assert(result.body.response.users[0]);
+            assert.equal(result.body.response.users[0].username, acsUsername);
+            assert(result.cookieString);
+            done();
+        });
+    });
+
+    before(function(done) {
+        acsApp.usersLogin({
+            login: acsUsername,
+            password: acsPassword
+        }, function(err, result) {
+            assert.ifError(err);
+            assert(result.body);
+            assert(result.body.meta);
+            assert.equal(result.body.meta.code, 200);
+            assert.equal(result.body.meta.method_name, 'loginUser');
+            assert(result.body.response);
+            assert(result.body.response.users);
+            assert(result.body.response.users[0]);
+            assert.equal(result.body.response.users[0].username, acsUsername);
+            assert(result.cookieString);
+            assert.equal(typeof result.cookieString, 'string');
+            acsApp.setSessionByCookieString(result.cookieString);
+            assert.equal(result.cookieString, acsApp.appOptions.cookieString);
+            done();
+        });
+    });
+
+    describe('Test clients geolocate', function() {
+        it('Should return clients geolocate info', function(done) {
+            acsApp.clientsGeolocate({
+                ip_address: "106.39.153.78"
+            }, function(err, result) {
+                assert.ifError(err);
+                assert(result);
+                assert(result.body);
+                assert(result.body.meta);
+                assert.equal(result.body.meta.code, 200);
+                assert.equal(result.body.meta.method_name, 'geolocateClient');
+                assert(result.body.response);
+                assert(result.body.response.ip_address);
+                geolocate_info = result.body.response.ip_address;
+                assert.equal(geolocate_info, '106.39.153.78');
+                assert(result.body.response.location);
+                assert(result.body.response.location.country_code);
+                assert(result.body.response.location.city);
+                assert(result.body.response.location.state);
+                assert(result.body.response.location.postal_code);
+                assert(result.body.response.location.latitude);
+                assert(result.body.response.location.longitude);
+                done();
+            });
+        });
+    });
+});
